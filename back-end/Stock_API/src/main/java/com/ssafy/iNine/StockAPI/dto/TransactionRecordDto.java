61fc5e82b8d0319b56e32586649ff41e8efd466f
이진호 <blueland1001@naver.com>
2023-09-25 11:20:42 +0900

    fix: builder에 allArgsConstructor 추가

--- back-end/Stock_API/src/main/java/com/ssafy/iNine/StockAPI/dto/TransactionRecordDto.java
+++ back-end/Stock_API/src/main/java/com/ssafy/iNine/StockAPI/dto/TransactionRecordDto.java
@@ -1,14 +1,13 @@
 package com.ssafy.iNine.StockAPI.dto;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
 @Builder
+@AllArgsConstructor
 public class TransactionRecordDto {
     private String prodName; // 종목명 (상품명, 거래가 발생한 상품의 명칭)
     private String prodCode; // 종목코드 (상품코드)
@@ -21,6 +20,6 @@ public class TransactionRecordDto {
     private double transAmt; // 거래금액 (해당종목이 본 거래에서 거래된 총 금액)
     private double settleAmt; // 정산금액 (거래대금에서 제세공과금 등을 차감한 금액)
     private double balanceAmt; // 거래후잔액 (해당 계좌의 현금 잔액(예수금))
-    private String currencyCode = "KRW"; // 거래에 적용된 통화코드 (거래에 적용된 통화코드 (ISO 4217 준용) ,통화코드값이 명시되어있지 않을 경우 KRW(원))
+    private String currencyCode = "KRW"; // 거래에 적용된 통화코드 (통화코드값이 명시되어있지 않을 경우 "KRW"(원))
     private String exCode; // 해외주식 거래소 코드
 }
