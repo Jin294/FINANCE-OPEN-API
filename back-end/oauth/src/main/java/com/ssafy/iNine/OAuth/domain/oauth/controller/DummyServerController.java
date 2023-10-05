0f2f4243bf58d96cb2c777f85f0c215619f72a96
홍유빈 <acrow0330@naver.com>
2023-10-05 14:42:30 +0900

    fix: cors 허용

--- back-end/oauth/src/main/java/com/ssafy/iNine/OAuth/domain/oauth/controller/DummyServerController.java
+++ back-end/oauth/src/main/java/com/ssafy/iNine/OAuth/domain/oauth/controller/DummyServerController.java
@@ -5,13 +5,11 @@ import kong.unirest.HttpResponse;
 import kong.unirest.Unirest;
 import org.apache.commons.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/oauth")
+@CrossOrigin("*")
 public class DummyServerController {
     @Value("${url.host}")
     private String hostUrl;
